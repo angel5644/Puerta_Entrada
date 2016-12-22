12606626f78c5ad450165e790be5dce4ce00751e
angel5644 <victor_a_75@hotmail.com>
2016-12-22 16:37:29 -0600

    fix deshabilitar mobile master page

--- Puerta_Entrada/App_Start/RouteConfig.vb
+++ Puerta_Entrada/App_Start/RouteConfig.vb
@@ -5,6 +5,8 @@ Public Module RouteConfig
     Sub RegisterRoutes(ByVal routes As RouteCollection)
         Dim settings As FriendlyUrlSettings = New FriendlyUrlSettings()
         settings.AutoRedirectMode = RedirectMode.Permanent
-        routes.EnableFriendlyUrls(settings)
+        'routes.EnableFriendlyUrls(settings)
+        ' Adding custom url handler
+        routes.EnableFriendlyUrls(settings, New Microsoft.AspNet.FriendlyUrls.Resolvers.IFriendlyUrlResolver() {New MyWebFormsFriendlyUrlResolver()})
     End Sub
 End Module
---
+++ Puerta_Entrada/Models/MyWebFormsFriendlyUrlResolver.vb
@@ -0,0 +1,18 @@
+﻿''' <summary>
+''' Hack to disable mobile master pages
+''' </summary>
+''' <remarks></remarks>
+Public Class MyWebFormsFriendlyUrlResolver
+    Inherits Microsoft.AspNet.FriendlyUrls.Resolvers.WebFormsFriendlyUrlResolver
+
+    ''' <summary>
+    ''' Disable ".Mobile.Master" master pages.
+    ''' </summary>
+    Protected Overrides Function TrySetMobileMasterPage(httpContext As HttpContextBase, page As UI.Page, mobileSuffix As String) As Boolean
+        If mobileSuffix = "Mobile" Then
+            Return False
+        Else
+            Return MyBase.TrySetMobileMasterPage(httpContext, page, mobileSuffix)
+        End If
+    End Function
+End Class
