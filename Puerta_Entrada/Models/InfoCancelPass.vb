136042c3e909fdf5afc2d1ca6f864a446c9db976
angel5644 <victor_a_75@hotmail.com>
2016-12-24 17:00:03 -0600

    clases para cancelar pase

---
+++ Puerta_Entrada/Models/InfoCancelPass.vb
@@ -0,0 +1,22 @@
+﻿Public Class InfoCancellPass
+    Private _P_InfoTrsp As InfoTrspCancelar
+    Public Property P_InfoTrsp() As InfoTrspCancelar
+        Get
+            Return _P_InfoTrsp
+        End Get
+        Set(value As InfoTrspCancelar)
+            _P_InfoTrsp = value
+        End Set
+    End Property
+
+    Private _P_Cursor As DataTable
+    Public Property P_Cursor() As DataTable
+        Get
+            Return _P_Cursor
+        End Get
+        Set
+            _P_Cursor = Value
+        End Set
+    End Property
+
+End Class
---
+++ Puerta_Entrada/Models/InfoTrspCancelar.vb
@@ -0,0 +1,32 @@
+﻿Public Class InfoTrspCancelar
+    Private _Trsp_Plate_Number As String
+    Public Property Trsp_Plate_Number() As String
+        Get
+            Return _Trsp_Plate_Number
+        End Get
+        Set
+            _Trsp_Plate_Number = Value
+        End Set
+    End Property
+
+    Private _Trsp_Call_Date As String
+    Public Property Trsp_Call_Date() As String
+        Get
+            Return _Trsp_Call_Date
+        End Get
+        Set(ByVal value As String)
+            _Trsp_Call_Date = value
+        End Set
+    End Property
+
+
+    Private _Trsp_Driver_Name As String
+    Public Property Trsp_Driver_Name() As String
+        Get
+            Return _Trsp_Driver_Name
+        End Get
+        Set(ByVal value As String)
+            _Trsp_Driver_Name = value
+        End Set
+    End Property
+End Class
